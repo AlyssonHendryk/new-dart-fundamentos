2300d851c3a02d68f169307dfb7354657f0f4c9e
Alysson Hendryk <90623152+AlyssonHendryk@users.noreply.github.com>
2023-10-06 13:14:41 -0300

    Add files via upload

---
+++ dart_fundamentos/lib/modulo3/10_enviar_email.dart
@@ -0,0 +1,11 @@
+class enviarEmail{
+
+  bool call(String email) {
+    print('chamando metodo call');
+    return enviar(email);
+  }
+  bool enviar(String email){
+    print('chamando metodo call');
+    return true;
+  }
+}
