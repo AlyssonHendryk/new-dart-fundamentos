803a2099c500341a57e07a4596b085cdbd4cc392
Alysson Hendryk <90623152+AlyssonHendryk@users.noreply.github.com>
2023-10-11 13:59:03 -0300

    Delete dart_fundamentos/lib/modulo1 directory

--- dart_fundamentos/lib/modulo1/3_condicionais.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  final idade = 19;
-
-  if(idade >= 18) {
-    print('pode tirar carteira de habilitação');
-  } else if (true){
-    print('nao pode tirar carteira de habilitação');
-  }
-}
